02f72b47ff0c99e59d680967a1748ba91405cac7
evhook <anonymous1986222@gmail.com>
2025-11-30 12:53:55 +0530

    Add BufferedReaderWrite example demonstrating file writing and reading

---
+++ BufferedReaderWrite.java
@@ -0,0 +1,22 @@
+import java.io.*;
+public class BufferedReaderWrite {
+    public static void main(String[] args){
+        try(BufferedWriter bfw = new BufferedWriter(new FileWriter("samaple2.txt"))){
+            bfw.write("This is a demo of using bufferedwriter and bufferedreader");
+            bfw.newLine();
+            bfw.write("for faster implementation.");
+            bfw.close();
+            System.out.println("Data is writtend into the successfully using bufferedwriter.");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        try(BufferedReader bfr = new BufferedReader(new FileReader("sample2.txt"))){
+            String line;
+            while((line=bfr.readLine())!=null)
+                System.out.println(line);
+            System.out.println("Data is printed on the terminal successfully.");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
