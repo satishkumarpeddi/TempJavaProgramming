2af50304cc4e5f9bb424f7baf3b9acfc4bf39986
evhook <anonymous1986222@gmail.com>
2025-11-30 18:31:45 +0530

    Add RaceCondition class to demonstrate thread safety issues with a shared counter

---
+++ RaceCondition.java
@@ -0,0 +1,27 @@
+class Counter{
+    int count;
+    public void countMethod(){
+        count++;
+    }
+}
+public class RaceCondition {
+    static Counter counter = new Counter();
+    public static void main(String[] args)throws Exception{
+        Thread t1 = new Thread(()->{
+            for(int i=0;i<1000;i++){
+                counter.countMethod();
+            }
+        });
+        Thread t2 = new Thread(()->{
+            for(int i=0;i<1000;i++){
+                counter.countMethod();
+            }
+        });
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println("Final Count  : "+counter.count);
+
+    }
+}
